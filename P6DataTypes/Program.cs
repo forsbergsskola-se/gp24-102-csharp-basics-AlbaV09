00c9af87bbfb6e234c729efd695458e625181696
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-16 15:03:43 +0200

    P6

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,6 @@
+﻿string outputString = "Hello World";
+Console.WriteLine(outputString);
+double decimValue = 0.3;
+Console.WriteLine(decimValue);
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
