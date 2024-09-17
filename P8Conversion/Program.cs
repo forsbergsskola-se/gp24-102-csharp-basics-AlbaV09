91dd0a6947637b79a5167fb647700a215368af5d
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-17 11:06:37 +0200

    P8

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Create a Console Project named P8Conversion How To? ok
+    Ask the user for a Number and assign the result to a variable.
+    Convert the variable to Type float.
+    Print the float to the Console.
+    Convert the double number to type int.
+    Print the int to the console.
+    Convert the original string to type int.
+    What happens? (Hint: You will see an Invalid Format Exception
+ */
+Console.WriteLine("Give me a number");
+string? number = Console.ReadLine();
+float numberFloat = float.Parse(number);
+Console.WriteLine(numberFloat);
+int numberWhole = (int) numberFloat;
+Console.WriteLine(numberWhole);
+int numberIntWhole = int.Parse(number);
