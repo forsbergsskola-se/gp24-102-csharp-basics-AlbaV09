9ba7870c8784b6b3a92ca954bddf634aa1e8c6a9
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-18 09:41:40 +0200

    P09_02

---
+++ P09_02MinutesToSeconds/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Give me a time in minutes");
+string? minutes = Console.ReadLine();
+int minutesNumber = int.Parse(minutes);
+int seconds = minutesNumber * 60;
+Console.WriteLine($"Your are talking about {seconds} seconds");
