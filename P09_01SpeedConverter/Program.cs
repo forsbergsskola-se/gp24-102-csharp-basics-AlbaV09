8bd2b2ac77cff9a25698d42a3887553e764d1db6
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-18 09:33:04 +0200

    P09_01

---
+++ P09_01SpeedConverter/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Give me your velocity in km/h");
+string? kmh = Console.ReadLine();
+float kmhNumber = float.Parse(kmh);
+float msconverter = 5f / 18f;
+float ms = kmhNumber * msconverter;
+int msfinal = Convert.ToInt32(ms);
+Console.WriteLine($"Your velocity is {kmhNumber} km/h what is the same as {msfinal} m/s");
