6f703f722eb4c8fd3cc3bbec3b4ecfc0eb05acae
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-18 10:35:42 +0200

    P09_10

---
+++ P09_10SecondsToMinutes/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Give me a number of seconds");
+string? seconds = Console.ReadLine();
+int secondsNumber = int.Parse(seconds);
+int minutes = secondsNumber / 60;
+int remainSec = secondsNumber % 60;
+Console.WriteLine($"{minutes} minute(s) and {remainSec} second(s)");
