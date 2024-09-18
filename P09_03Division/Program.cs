765fc1b3b418ad2c2f9c5bc81f7b5587ff33a00c
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-18 09:59:21 +0200

    P09_03

---
+++ P09_03Division/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Give me an integer number");
+string? first = Console.ReadLine();
+Console.WriteLine("Give me another integer number");
+string? second = Console.ReadLine();
+float firstNumber = float.Parse(first);
+float secondNumber = float.Parse(second);
+float division = firstNumber / secondNumber;
+Console.WriteLine($"The result of {firstNumber} divided with {secondNumber} is {division}");
