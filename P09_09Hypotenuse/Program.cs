298e022e6113fce504cb0206de6bd810c333d7fb
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-18 10:30:42 +0200

    P09_09

---
+++ P09_09Hypotenuse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("I will calculate for you the hypotenuse of a right triangle. Give me the length of one side");
+string? first = Console.ReadLine();
+Console.WriteLine("Give me the other");
+string? second = Console.ReadLine();
+float firstNumber = float.Parse(first);
+float secondNumber = float.Parse(second);
+float hypo = (firstNumber * firstNumber) + (secondNumber * secondNumber);
+Console.WriteLine($"Your hypotenuse is {Math.Round(Math.Sqrt(hypo))}");
+
