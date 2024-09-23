7f4f36ae76939a9de1f4a317aac1a5dc3ed3fb62
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 09:40:51 +0200

    P11_3

---
+++ P11_3Characters/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Develop a program that asks the user to enter a single character and then determines whether it's a digit, a vowel or a consonant.
+ Display an appropriate message.
+
+    Input: 'a'
+    Output: "That's a vowel."
+ */
+Console.WriteLine("Give me a character");
+string character = Console.ReadLine();
+//int characterNumber = int.Parse(character);
+if (character == "A" || character == "a" || character == "E" || character == "e" || character == "I" || character == "i" || character == "O" || character == "o" || character == "U" || character == "o"){
+Console.WriteLine("That's a vowel");
+} 
+else {
+Console.WriteLine("That's a consonant");
+}
+
