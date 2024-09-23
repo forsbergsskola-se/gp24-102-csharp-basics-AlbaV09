0b31e5aa7f2f7ad94b6f15cb26c69f30e7d4dc77
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 11:33:10 +0200

    P12_4

---
+++ P12_4Random_Chance/Program.cs
@@ -0,0 +1,9 @@
+﻿// Write a program that has a 10% chance of showing a secret message.
+// Otherwise it just displays a message saying to try again.
+
+double chance = 0.1d;
+if(Random.Shared.NextDouble() < chance){
+    Console.WriteLine("Congratulations! You can read the secret message, you can find it behind the door!");
+} else {
+    Console.WriteLine("Try again!");
+}
