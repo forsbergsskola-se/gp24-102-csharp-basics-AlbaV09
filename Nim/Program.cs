8ab33723d34e857b7f352e58fe04200303846270
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 14:44:51 +0200

    Nim

---
+++ Nim/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Welcome to Nim!");
+int matches = 24;
+string draw = "||||||||||||||||||||||||";
+Console.WriteLine($"{draw} ({matches})");
+Console.WriteLine("How many matches do you want to draw?");
+
+Play:
+int userInput = Convert.ToInt32(Console.ReadLine());
+if (userInput == 1 || userInput == 2 || userInput == 3){
+matches = matches - userInput;
+Console.WriteLine($"The number of matches is {matches}");
+int aiChoice = Random.Shared.Next(1,4);
+Console.WriteLine($"The AI draws {aiChoice} matches");
+matches = matches - aiChoice;
+Console.WriteLine($"The number of matches is {matches}");
+if (matches > 0)
+{
+    Console.WriteLine("Your turn");
+    goto Play; 
+}
+}
+else
+{
+    Console.WriteLine("Error, just 1, 2 or 3, try again.");
+    goto Play;
+}
+
