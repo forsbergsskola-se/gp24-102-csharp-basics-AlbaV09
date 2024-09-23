ed39ac7c8afb66ccd88ca74fc9a63e050fa592f1
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 09:56:40 +0200

    P11_5

---
+++ P11_5EvenOrOdd/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Write a program that checks if a given integer is even or odd and displays an appropriate message.
+
+    Input: 12
+    Output: "That's even."
+ */
+
+Console.WriteLine("give me a number");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+if (numberInt % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
