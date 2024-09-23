3966c3d64488c22c1a8327dbfc28bbc7424d589c
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 11:23:53 +0200

    P12_3

---
+++ P12_3Random_Item/Program.cs
@@ -0,0 +1,5 @@
+﻿// Write a program that simulates item drops in a game. Define five different items, and randomly select an item for the player to acquire.
+Random random = new Random();
+string[] items = ["stone", "crystal", "seed", "water", "feather"];
+int randomItemIndex = random.Next(0, items.Length);
+Console.WriteLine(items[randomItemIndex]);
