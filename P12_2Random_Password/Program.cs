ea89f4e768cd0b765b866d0efe7d3c55a4b71167
Alba Verdaguer Farell <alba.verdaguer.farell@student.forsbergsskola.se>
2024-09-23 11:14:17 +0200

    P12_2

---
+++ P12_2Random_Password/Program.cs
@@ -0,0 +1,11 @@
+﻿// Design a program that generates a random password with 6 characters, digits or symbols.
+
+Random random = new Random();
+int password = random.Next(0, 10);
+int password2 = random.Next(0, 10);
+int password3 = random.Next(0, 10);
+int password4 = random.Next(0, 10);
+int password5 = random.Next(0, 10);
+int password6 = random.Next(0, 10);
+
+Console.WriteLine($"Your new password is {password}{password2}{password3}{password4}{password5}{password6}");
